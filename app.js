5b0f4949e32f0b98f888f1fd2945729670757783
alidanish786 <71668488+alidanish786@users.noreply.github.com>
2024-11-13 19:39:18 +0530

    add geocode code
    add geocode

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const request=require('request');
 
-const url='https://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233';
+const url='https://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233&units=f';
 
-request({url:url,json:true},(error,response)=>{
+// request({url:url,json:true},(error,response)=>{
 
-    //const data=JSON.parse(response.body);
-    //console.log(response.body)
+//     //const data=JSON.parse(response.body);
+//     //console.log(response.body)
 
-    var data=response.body.current;
+//     var data=response.body.current;
 
-    console.log('Current temperature is '+data.temperature +', but it feels like '+data.feelslike);
-    console.log('Current temperature is '+data.temperature +', but it feels like '+data.feelslike);
+//     console.log('Current temperature is '+data.temperature +', but it feels like '+data.feelslike);
+//     console.log('Current temperature is '+data.temperature +', but it feels like '+data.feelslike);
 
+// })
+
+const geoCode='https://api.mapbox.com/search/geocode/v6/forward?q=Los%20Angeles&access_token=pk.eyJ1IjoiaGl0bWFuMjAwNiIsImEiOiJja28ybWN2ZjYwM2RnMndtdXd3cHN1OXg5In0.8VpR-poOVftYQB8X2C1arA';
+
+request({url:geoCode,json:true},(error,response)=>{
+    const latitude=response.body.features[0].properties.coordinates.latitude;
+    const longitude=response.body.features[0].properties.coordinates.longitude;
+
+    console.log('The latitude is: '+latitude+' and longitude is: '+longitude)
 })
