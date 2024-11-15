7a4c0e93528140ac53ae2724f32baababf11d946
alidanish786 <71668488+alidanish786@users.noreply.github.com>
2024-11-15 12:13:30 +0530

    adding geocode code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const geocode=require('./utils/geocode');
 
 // const url='https://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233&units=f';
 
@@ -17,28 +18,35 @@ const request = require("request");
 //     }
 // })
 
-const geoCode =
-  "https://api.mapbox.com/search/geocode/v6/forward?q=&&&66&access_token=pk.eyJ1IjoiaGl0bWFuMjAwNiIsImEiOiJja28ybWN2ZjYwM2RnMndtdXd3cHN1OXg5In0.8VpR-poOVftYQB8X2C1arA";
+// const geoCode =
+//   "https://api.mapbox.com/search/geocode/v6/forward?q=&&&66&access_token=pk.eyJ1IjoiaGl0bWFuMjAwNiIsImEiOiJja28ybWN2ZjYwM2RnMndtdXd3cHN1OXg5In0.8VpR-poOVftYQB8X2C1arA";
 
-request({ url: geoCode, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to process request..");
-  } else if (response.body.features.length==0) {
-    console.log(
-      "Error code:" +
-        response.body.error +
-        ".Unable to process with given input.. try another search"
-    );
-  } else {
+// request({ url: geoCode, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to process request..");
+//   } else if (response.body.features.length==0) {
+//     console.log(
+//       "Error code:" +
+//         response.body.error +
+//         ".Unable to process with given input.. try another search"
+//     );
+//   } else {
 
-    console.log(response.body);
+//     console.log(response.body);
 
-    const latitude = response.body.features[0].properties.coordinates.latitude;
-    const longitude =
-      response.body.features[0].properties.coordinates.longitude;
+//     const latitude = response.body.features[0].properties.coordinates.latitude;
+//     const longitude =
+//       response.body.features[0].properties.coordinates.longitude;
 
-    console.log(
-      "The latitude is: " + latitude + " and longitude is: " + longitude
-    );
-  }
-});
+//     console.log(
+//       "The latitude is: " + latitude + " and longitude is: " + longitude
+//     );
+//   }
+// });
+
+
+
+geocode('Tokyo',(error,data)=>{
+    console.log(data)
+
+})
---
+++ utils/geocode.js
@@ -0,0 +1,25 @@
+const request=require('request');
+
+const geocode=(address,callback)=>{
+    const url='https://api.mapbox.com/search/geocode/v6/forward?q='+address+'&access_token=pk.eyJ1IjoiaGl0bWFuMjAwNiIsImEiOiJja28ybWN2ZjYwM2RnMndtdXd3cHN1OXg5In0.8VpR-poOVftYQB8X2C1arA';
+    if(address){
+
+        request({url:url,json:true},(error,response)=>{
+            if(error){
+                callback('Error while connecting Geoservice',undefined);
+            }
+            else if(response.body.features.length==0){
+                callback('Unable to process with given input.. try another search',undefined)
+            }
+            else{
+                callback(undefined,{
+                    latitude: response.body.features[0].properties.coordinates.latitude,
+                    longitude:response.body.features[0].properties.coordinates.longitude,
+                    location:response.body.features[0].place_name
+                });               
+            }
+        });
+    }
+}
+
+module.exports=geocode;
