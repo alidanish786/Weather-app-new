9134d47225626d1d281839396799c4d62e234593
alidanish786 <71668488+alidanish786@users.noreply.github.com>
2024-11-20 23:11:54 +0530

    adding handlebars code

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,13 +4,33 @@ const express=require('express');
 
 const app=express();
 const publicDirPath=path.join(__dirname,'../public');
+// app.set('views', path.join(__dirname, '../views'));
+
+app.set('view engine','hbs');
 
 app.use(express.static(publicDirPath));
 
 app.get('',(req,res)=>{
-    res.send('Hello express!');
+    // res.send('Hello express!');
+    res.render('index',{
+        title:'Heading',
+        name:'Created by me'
+    });
 })
 
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        name:'About',
+        createdby:'Created by Danish'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        name:'Danish',
+        createdby:'Kane'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
